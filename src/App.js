09534ef4e7204b9c140632d8ae0178a3718ebc5f
Miheretab Alemu <mihrtab@gmail.com>
2021-01-20 06:59:37 +0300

    change on layout and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import React, { useState } from 'react';
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter
+} from 'reactstrap';
+
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+
+const categories = ['view-all-pranks', 'new-prank-calls', 'food-restaurant-prank-calls'];
 
 function App() {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {categories.map((category) =>
+      <Card>
+        <CardBody onClick={toggle}>
+          <CardTitle tag="h5">{category}</CardTitle>
+        </CardBody>
+
+        <Modal isOpen={modal} toggle={toggle} >
+          <ModalHeader toggle={toggle}>{category}</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
+      </Card>
+      )}
     </div>
   );
 }
